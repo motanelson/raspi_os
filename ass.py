05aaf92ef46d4d049fa324faebf3242e3d065008
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-12-08 13:16:07 +0000

    Add files via upload

--- ass.py
+++ ass.py
@@ -3,28 +3,16 @@ print("\033c\033[43;30m\nget me file name to show")
 a=input()
 print("\nget me function name to show")
 b=input()
-c="\"C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v10.0A\\bin\\NETFX 4.8.1 Tools\\"\\ildasm.exe /tok /byt %1 /out=myfile.il".replace("%1",a)
+c="copy %1 \"C:\\temp\"".replace("%1",a)
 os.system(c)
-f1=open("myfile.il","r")
+os.chdir("C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v10.0A\\bin\\NETFX 4.8.1 Tools")
+c="ildasm.exe %1 /out=c:/temp/myfile.il".replace("%1",a)
+os.system(c)
+f1=open("c:/temp/myfile.il","r")
 d=f1.read()
 f1.close()
 e=d.split("\n")
 i=False
 for h in e:
     h=h.strip()
-    if i:
-        
-        o=h.find(">:")
-        if o>-1:
-            i=False
-    if i:
-        print(h)
-    j=h.find(">:")
-    if j>-1:
-        l=h.find(b)
-        if l>-1:
-            j=h.find("<")
-            i=True
-            print(h[j+1:])
-     
-        
+    print(h)
